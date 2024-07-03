e8a84847a7491527a970387c0cdda4fd74ad53d9
JohaneDecamps <johane.decamps@gmail.com>
2024-07-03 18:07:06 +0200

    fix: schema sql

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,44 +1,42 @@
--- SQLBook: Code
 
 CREATE TABLE user (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE entreprise (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
     description TEXT NOT NULL,
     detail TEXT NOT NULL,
     location VARCHAR(100) NOT NULL,
-    activity_sector VARCHAR(100) NOT NULL,
+    sector VARCHAR(100) NOT NULL,
     creation DATE NOT NULL
 );
-
-CREATE TABLE job_offer (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE category (
+    id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    name VARCHAR(255) NOT NULL
+);
+CREATE TABLE job (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     title VARCHAR(80) NOT NULL,
     description TEXT NOT NULL,
     skill VARCHAR(60) NOT NULL,
-    contract_type VARCHAR(60) NOT NULL,
-    publication date DATE NOT NULL,
-    rqth BOOLEAN NOT NULL,
+    contract VARCHAR(60) NOT NULL,
+    date DATE NOT NULL,
+    rqth BOOLEAN NOT NULL DEFAULT FALSE,
     location VARCHAR(80) NOT NULL,
     salary VARCHAR(80) NOT NULL,
     status VARCHAR(80) NOT NULL,
-    category_id INT UNSIGNED,
+    category_id  INT UNSIGNED,
     entreprise_id INT UNSIGNED,
     FOREIGN KEY (category_id) REFERENCES category (id),
-    FOREIGN KEY (entreprise_id) REFERENCES entreprise (id)
+    FOREIGN KEY (entreprise_id) REFERENCES entreprise (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     name VARCHAR(60)
 );
 
-CREATE TABLE category (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(255) NOT NULL
-)
