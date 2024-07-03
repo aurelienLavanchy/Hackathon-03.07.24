03014f647bd413b8639835f34d0d922152b36d10
Aurélien Lavanchy <lavanchy.aurelien@gmail.com>
2024-07-03 15:53:22 +0200

    fix: missing navbar in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   const [currentFont, setCurrentFont] = useState();
 
   return (
     <main>
+      <Navbar />
       <h1>temp</h1>
       <Outlet context={{ currentFont, setCurrentFont }} />
     </main>
