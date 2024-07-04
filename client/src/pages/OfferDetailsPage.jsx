f67b825ff0d52916b78afd19e1c8ef4a5fe30435
Aurélien Lavanchy <lavanchy.aurelien@gmail.com>
2024-07-04 11:39:28 +0200

    style: offer details

--- client/src/pages/OfferDetailsPage.jsx
+++ client/src/pages/OfferDetailsPage.jsx
@@ -1,41 +1,37 @@
 import { useLoaderData, Link } from "react-router-dom";
+import "./OfferDetailsPage.css";
 
 export default function OfferDetailsPage() {
   const [jobOffer] = useLoaderData();
-  console.info(jobOffer);
-  // title, description, skill, contract, date, rqth, location, salary, status
+  /* console.info(jobOffer); */
+  const postDate = new Date(jobOffer.date);
+  const formattedDate = postDate.toISOString().substring(0, 10);
+
   return (
-    <>
-      <h1>L'offre que vous avez sélectionnée</h1>
-      <div>
+    <div className="offer-container">
+      <h1 className="offer-title">L'offre que vous avez sélectionnée</h1>
+      <div className="offer-content">
         <header>
           <h2>{jobOffer.title}</h2>
         </header>
-        <div>
+        <div className="offer-details">
           <p>{`Lieu: ${jobOffer.location}`}</p>
           <p>{`Type de contrat: ${jobOffer.contract}`}</p>
           <p>{`Salaire: ${jobOffer.salary}`}</p>
         </div>
-        <p>{`Les missions pour ce poste: ${jobOffer.description}`}</p>
+        <p className="offer-description">{`Les missions pour ce poste: \n${jobOffer.description}`}</p>
         <footer>
-          <p>{`Posté le ${jobOffer.date}`}</p>
-          {jobOffer.status === "active" ? (
-            <>
-              <p>{`Status: ${jobOffer.status}`}</p>
-              <Link to={`/entreprise/${jobOffer.entreprise_id}`}>
-                En savoir plus sur l'entreprise
-              </Link>
-            </>
-          ) : (
-            <>
-              <p>Cette offre n'est plus valable</p>
-              <Link to={`/entreprise/${jobOffer.entreprise_id}`}>
-                Voir les autres offres de cette entreprise
-              </Link>
-            </>
-          )}
+          <div>
+            <p>{`Posté le ${formattedDate}`}</p>
+            <p>{`Status: ${jobOffer.status}`}</p>
+          </div>
+          <Link to={`/entreprise/${jobOffer.company_id}`}>
+            {jobOffer.status === "active"
+              ? "En savoir plus sur l'entreprise"
+              : "Voir les autres offres de cette entreprise"}
+          </Link>
         </footer>
       </div>
-    </>
+    </div>
   );
 }
