cefd017147cffd50ad15f96588c082e2b13f358b
JohaneDecamps <johane.decamps@gmail.com>
2024-07-04 11:18:37 +0200

    fix: intro page

---
+++ client/src/pages/intro/IntroPage.jsx
@@ -0,0 +1,17 @@
+import {Link} from "react-router-dom";
+import logo from "../../assets/images/logo.png"
+import fleche from "../../assets/images/fleche.png"
+
+import "./Intro.css";
+
+export default function IntroPage() {
+    return (
+
+    <div className="container-intro"> 
+
+    <img className="logo-intro" src={logo} alt="logo"/>
+     <h1 className="text-intro">Le site des offres d'emploi qui rendent la tech + inclusive </h1>
+   <Link to="/home"> <img className="fleche-intro" src={fleche} alt="suivant"/> </Link>
+</div>
+    );
+  }
