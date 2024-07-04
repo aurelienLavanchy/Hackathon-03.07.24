ce219f7857bcabe7dffbb29e8a2601729b402441
angelique888 <angeliquegrall5@gmail.com>
2024-07-04 21:50:21 +0200

    feat: SafeZone

--- client/src/pages/SafeZonePage.jsx
+++ client/src/pages/SafeZonePage.jsx
@@ -1,8 +1,27 @@
 export default function TipsPage() {
   return (
     <>
-      <h1>Conseils</h1>
-      <p>contenu</p>
+      <h1>L'Accessibilité Numérique: Vers une Tech Inclusive</h1>
+      <p>
+        L'accessibilité numérique est à la fois un défi et une opportunité pour l'industrie de la tech. En s'assurant que les produits technologiques soient accessibles à tous, les entreprises peuvent non seulement répondre à des exigences éthiques et légales, mais aussi se démarquer sur le marché. 
+        <a href="https://www.kaliop.com/fr/accessibilite-numerique-comment-le-design-inclusif-repond-au-handicap/" target="_blank" rel="noopener noreferrer">
+          Lire plus
+        </a>
+      </p>
+      <h1>L'Inclusivité dans la Tech: Promouvoir la Participation des Femmes</h1>
+      <p>
+        L'industrie technologique, autrefois dominée par les hommes, évolue pour devenir plus inclusive. L'un des aspects les plus critiques de cette transformation est l'augmentation de la participation des femmes dans le secteur de la tech. Cependant, malgré les progrès réalisés, des obstacles subsistent, et il est crucial de continuer à promouvoir une culture d'inclusivité. 
+        <a href="https://weborama.com/2023/11/favoriser-linclusion-des-femmes-dans-la-tech-et-la-data-un-imperatif-pour-lavenir-de-notre-industrie/" target="_blank" rel="noopener noreferrer">
+          Lire plus
+        </a>
+      </p>
+      <h1>Entreprises Ouvertes d'Esprit dans la Tech: Un Engagement pour la Diversité et l'Inclusion</h1>
+      <p>
+        Dans un monde où l'innovation est essentielle, les entreprises technologiques qui embrassent la diversité et l'inclusion se démarquent. Ces entreprises comprennent que des équipes diversifiées, représentant différentes perspectives et expériences, sont cruciales pour résoudre des problèmes complexes et développer des produits innovants. Elles ouvrent leurs portes à la différence, créant des environnements où chacun se sent valorisé et capable de contribuer pleinement.
+        <a href="https://courriercadres.com/inclusion-et-diversite-dans-la-tech-lexemple-de-qonto/" target="_blank" rel="noopener noreferrer">
+          Lire plus
+        </a>
+      </p>
     </>
   );
 }
