262742ee05763a4e35922a03933b4335040208f3
Aurélien Lavanchy <lavanchy.aurelien@gmail.com>
2024-07-04 11:50:29 +0200

    fix: removed navbar in intro page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   const [currentFont, setCurrentFont] = useState();
 
   return (
     <main>
+      <Navbar />
       <Outlet context={{ currentFont, setCurrentFont }} />
     </main>
   );
